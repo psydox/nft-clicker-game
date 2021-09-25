781db84776eeff3dafe820c02c6cc0f1fe96232e
MarcoPereira27 <marco_epav@hotmail.com>
2021-09-26 00:03:55 +0100

    fixed news

--- src/components/News.js
+++ src/components/News.js
@@ -1,26 +1,30 @@
 import React, { useState, useEffect } from 'react';
-import Ticker from 'react-ticker';
+import Marquee from 'react-fast-marquee';
 
 export default function News() {
   const NEWS = [
-    'CHINA BANS BITCOIN',
-    'CHINA BANS BITCOIN MINING',
-    'SIMÃO FROM ESMAE JOINS A PUMP AND DUMP SCHEME',
-    'DIOGO VERÍSSIMO SELLS HIS FIRST NFT COLLECTION FOR OVER 50ETH',
-    'PEDRO AND MARCO, FOUNDERS OF APPLE 2, ARE JOINING THE NFT SPACE, FOUND OUT WHY!',
-    'MAN FROM PORTO STEALS 500 HELIUM MINERS',
-    'GRETUA JOINS THE FRENZY AND ADDS 30BTC TO ITS FINANCIAL HOLDINGS',
+    'CHINA BANS BITCOIN      ',
+    'CHINA BANS BITCOIN MINING      ',
+    'SIMÃO FROM ESMAE JOINS A PUMP AND DUMP SCHEME      ',
+    'DIOGO VERÍSSIMO SELLS HIS FIRST NFT COLLECTION FOR OVER 50ETH      ',
+    'PEDRO AND MARCO, FOUNDERS OF APPLE 2, ARE JOINING THE NFT SPACE, FIND OUT WHY!      ',
+    'MAN FROM PORTO STEALS 500 HELIUM MINERS      ',
+    'GRETUA JOINS THE FRENZY AND ADDS 30BTC TO ITS FINANCIAL HOLDINGS      ',
   ];
 
   return (
     <div className='news'>
-      <Ticker mode="smooth">
-        {({ index }) => (
-          <>
-            <p>{NEWS[index]}</p>
-          </>
-        )}
-      </Ticker>
+      <Marquee gradient={false}>
+        {NEWS.map((n) => (
+          <p>
+            {n}
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+          </p>
+        ))}
+      </Marquee>
     </div>
   );
 }
